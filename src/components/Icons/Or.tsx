5a73efc38e4c6c0d39c26e87576fca9fe553e9f0
Adly <adly.abdullah@mmob.com>
2024-07-02 18:45:26 +0800

    Refine OR svg

--- src/components/Icons/Or.tsx
+++ src/components/Icons/Or.tsx
@@ -12,10 +12,10 @@ const Or = () => (
     <g id="layer1">
       <path
         id="rect1"
-        d="m 9,1 c 4,6 4,12 0,18 h 7 c 5,0 13,-5 13,-9 0,-4 -8,-9 -13,-9 z"
+        d="m 8,1 c 4,6 4,12 0,18 h 8 c 5,0 13,-5 13,-9 0,-4 -8,-9 -13,-9 z"
       />
-      <path d="M 0,6 H 11" id="path1" />
-      <path d="M 0,15 H 11" id="path1-7" />
+      <path d="M 0,6 H 10" id="path1" />
+      <path d="M 0,15 H 10" id="path1-7" />
       <path d="M 29,10 H 35" id="path1-9" />
     </g>
   </svg>
