1d1e0880421bcc03de47d2098dd33ad1d6503027
slebetman <slebetman@gmail.com>
2024-07-08 20:00:51 +0800

    Fix reference issue

--- src/lib/chartRefs.ts
+++ src/lib/chartRefs.ts
@@ -2,6 +2,10 @@ import { Chart } from 'hooks/useChart';
 
 let chartRef: Chart = { nodes: [], edges: [], modules: [] };
 
-export const setChartRef = (ref: Chart) => (chartRef = { ...ref });
+export const setChartRef = (ref: Chart) => {
+	chartRef.nodes = ref.nodes;
+	chartRef.edges = ref.edges;
+	chartRef.modules = ref.modules;
+};
 
 export const getChartRef = () => chartRef;
