e799cabcdc919a752dc5b89a5894bee8b7f0295e
slebetman <slebetman@gmail.com>
2024-07-18 22:36:35 +0800

    Removed commented out code

--- src/pages/[...fileName]/index.tsx
+++ src/pages/[...fileName]/index.tsx
@@ -26,8 +26,6 @@ const Home: NextPage = () => {
 				/>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-
-			{/* <header className={styles.header}>React Flow - Next.js Example</header> */}
 			<CircuitEditor fileName={path.join(...fileName)} />
 		</div>
 	);
